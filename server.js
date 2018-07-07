9ee629a3f0648adcf80d5203231b6fab74579d11
dracco1993 <dracco1993@gmail.com>
2018-07-07 14:40:12 -0400

    Welp, we can now detect presence changes...
    More to come as I learn more of the API. ��

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("Booting complete!");
+  console.log("Bot online!");
 });
 
 client.on("message", (message) => {
@@ -12,4 +13,12 @@ client.on("message", (message) => {
   }
 });
 
+client.on("presenceUpdate", (oldMember, newMember) => {
+  const presence = newMember.presence
+  if (presence.status == "online" && presence.game != null) {
+    console.log(newMember.displayName, " is now playing:", presence.game.name);
+  }
+});
+
+console.log("Booting up...");
 client.login(config.token);
