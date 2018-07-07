9a15be9904224b7a3d51412981a0932ca43d5b3a
dracco1993 <dracco1993@gmail.com>
2018-07-07 16:10:48 -0400

    Just a little clean up

--- server.js
+++ server.js
@@ -14,13 +14,15 @@ client.on("message", (message) => {
   }
 });
 
-client.on("presenceUpdate", (oldMember, newMember) => {
+client.on("presenceUpdate", (_, newMember) => {
+  // Don't respond to things from other bots
   if (newMember.user.bot === true) {
     return
   }
+
   const presence = newMember.presence
   if (presence.status == "online" && presence.game != null) {
-    console.log(newMember.displayName, " is now playing:", presence.game.name);
+    console.log(newMember.displayName, "is now playing:", presence.game.name);
   }
 });
 
