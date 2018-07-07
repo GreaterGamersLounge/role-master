1210762c8f5c3b950ff9a9c4ed1eff3a054f9152
dracco1993 <dracco1993@gmail.com>
2018-07-07 15:38:20 -0400

    Don't let other bots talk with us... >:)

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Global objects
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
@@ -14,6 +15,9 @@ client.on("message", (message) => {
 });
 
 client.on("presenceUpdate", (oldMember, newMember) => {
+  if (newMember.user.bot === true) {
+    return
+  }
   const presence = newMember.presence
   if (presence.status == "online" && presence.game != null) {
     console.log(newMember.displayName, " is now playing:", presence.game.name);
