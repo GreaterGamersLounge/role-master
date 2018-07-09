a60a279f5cca12ed8c86cd3a0fc7632b0a01f6d2
dracco1993 <dracco1993@gmail.com>
2018-07-09 01:24:04 -0400

    Added the RoleMaster, the master of the roles.  :D

---
+++ role-master.js
@@ -0,0 +1,38 @@
+class RoleMaster {
+  constructor(user, roleName) {
+    this.user = user;
+    this.roleName = roleName;
+
+    console.log("I AM THE ROLE MASTER!");
+  }
+
+  attemptRoleAdd() {
+    // Grab the potential role we want to add
+    const role = this.getRole();
+
+    // If the correct role exists already
+    if (role !== null) {
+      console.log("GOT IT!");
+      this.addRole(role);
+    } else {
+      // Ask the admins to add it or something.  Soon™
+      console.log(`Sorry, I could not find role: ${this.roleName}`);
+      console.log("<sadface>");
+    }
+  }
+
+  addRole(role) {
+    // Maybe want to return if this worked or not later on?
+    this.user.addRole(role).catch(console.error);
+  }
+
+  getRole() {
+    return this.user.guild.roles.find("name", this.roleName);
+  }
+
+  removeRole() {
+    // member.removeRole(role).catch(console.error);
+  }
+}
+
+module.exports = RoleMaster;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const RoleMaster = require("./role-master.js");
 
 client.on("ready", () => {
   console.log("Booting complete!");
@@ -22,6 +23,8 @@ client.on("presenceUpdate", (_, newMember) => {
 
   const presence = newMember.presence
   if (presence.status == "online" && presence.game != null) {
+    const rm = new RoleMaster(newMember, presence.game.name);
+    rm.attemptRoleAdd();
     console.log(newMember.displayName, "is now playing:", presence.game.name);
   }
 });
