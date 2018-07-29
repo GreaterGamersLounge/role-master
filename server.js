bd82b00d457fdb5fb217a37bdfaa8d3eb5a14e62
dracco1993 <dracco1993@gmail.com>
2018-07-29 01:39:53 -0400

    Added message to admins if a role doesn't exist
    Closes #3

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ client.on("message", (message) => {
 client.on("presenceUpdate", (_, newMember) => {
   if (newMember.isBot === true) return
 
-  const presence = newMember.presence
+  const presence = newMember.presence;
   if (presence.status == "online" && presence.game != null) {
-    const rm = new RoleMaster().attemptRoleAdd(newMember, presence.game.name);
+    new RoleMaster().attemptRoleAdd(newMember, presence.game.name);
     console.log(newMember.displayName, "is now playing:", presence.game.name);
   }
 });
--- src/RoleMaster.js
+++ src/RoleMaster.js
@@ -14,13 +14,19 @@ class RoleMaster {
       console.log("GOT IT!");
       this.addRole(user, role);
     } else {
-      // Ask the admins to add it or something.  Soon™
-      console.log(`Sorry, I could not find role: ${roleName}`);
-      console.log("<sadface>");
+      // Ask the admins to add the new game role
+      const channel = user.guild.channels.find('name', config.adminChannelName);
+      if (channel !== null) {
+        channel.send(`
+          ${user.displayName} is now playing ${roleName}, but we don't have a role for it... Add it with "!cgr ${roleName}"
+        `);
+      };
     }
   }
 
   addRole(user, role) {
+    // Check to see if the user already has that role
+
     // Maybe want to return if this worked or not later on?
     user.addRole(role).catch(console.error);
     console.log(`Role "${role.name}" given to ${user.displayName}.`);
